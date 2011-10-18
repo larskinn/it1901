3bdd43e7974eb80c7d7a8db6d0112bf74f54527e
Dimmy <dimbnd@gmail.com>
2011-10-19 00:34:55 +0200

    Removed training wheels for the database.
    Had to add a listener to OrderWindow in order to close the database only
    after the program has been closed, and not right after the window has
    been instantiated. Sorry for the mess, Leo :x

--- order_system/src/ntnu/it1901/gruppe4/gui/ordergui/OrderWindow.java
+++ order_system/src/ntnu/it1901/gruppe4/gui/ordergui/OrderWindow.java
@@ -9,6 +9,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,18 +30,16 @@ public class OrderWindow implements ActionListener {
 	private JPanel currentPanel;
 	private OrderSummary orderSummary;
 	private ResizeListener resizeListener;
-	
+
 	private class ResizeListener extends ComponentAdapter {
 		public void componentResized(ComponentEvent e) {
-			//Dynamically resize the western and eastern panels
+			// Dynamically resize the western and eastern panels
 			currentPanel.setPreferredSize(new Dimension(
-					(int)(frame.getWidth() * 0.6666),
-							frame.getHeight()));
-			
+					(int) (frame.getWidth() * 0.6666), frame.getHeight()));
+
 			orderSummary.setPreferredSize(new Dimension(
-					(int)(frame.getWidth() * 0.3333),
-							frame.getHeight()));	
-			
+					(int) (frame.getWidth() * 0.3333), frame.getHeight()));
+
 			currentPanel.revalidate();
 		}
 	}
@@ -49,10 +50,14 @@ public class OrderWindow implements ActionListener {
 
 	public static void main(String[] args) {
 		DataAPI.open("./data.db");
-		DataAPI.clearDatabase();
-		DataAPI.createExampleData();
-		
+		// DataAPI.clearDatabase();
+		// DataAPI.createExampleData();
+
 		new OrderWindow();
+	}
+
+	private static void cleanup() {
+		//	This would make more sense if DataAPI was non-static.
 		DataAPI.close();
 	}
 
@@ -64,90 +69,124 @@ public class OrderWindow implements ActionListener {
 		customerPanel = new CustomerPanel(orderSummary);
 		buttonPanel = new ButtonPanel(this);
 		resizeListener = new ResizeListener();
-		
+
 		orderSummary.addOrderListener(orderHistoryPanel);
 		orderSummary.addComponentListener(resizeListener);
 		menuSearchPanel.addComponentListener(resizeListener);
 		customerPanel.addComponentListener(resizeListener);
 		orderHistoryPanel.addComponentListener(resizeListener);
-		
+
 		frame.setSize(Layout.initialSize);
 		frame.setLayout(new BorderLayout());
 		frame.add(orderSummary, BorderLayout.EAST);
 		frame.add(buttonPanel, BorderLayout.SOUTH);
 		changeView(View.MENU);
-		
+
 		frame.setTitle("Bestillingsvindu");
-		frame.setLocationRelativeTo(null); //Center the frame
-		
-		//Adds a global key listener
-		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+		frame.setLocationRelativeTo(null); // Center the frame
+
+		// Adds a global key listener
+		KeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.addKeyEventDispatcher(new KeyEventDispatcher() {
+					@Override
+					public boolean dispatchKeyEvent(KeyEvent e) {
+						switch (e.getKeyCode()) {
+						case KeyEvent.VK_F1:
+							changeView(View.MENU);
+							break;
+						case KeyEvent.VK_F2:
+							changeView(View.CUSTOMER);
+							break;
+						case KeyEvent.VK_F3:
+							changeView(View.HISTORY);
+							break;
+						case KeyEvent.VK_ESCAPE:
+							System.exit(0);
+							break;
+						}
+						return false;
+					}
+				});
+
+		// Change to dispose or close when called from another frame (ie. the
+		// splash screen)
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+
+		frame.addWindowListener(new WindowListener() {
 			@Override
-			public boolean dispatchKeyEvent(KeyEvent e) {
-				switch (e.getKeyCode()) {
-					case KeyEvent.VK_F1:
-						changeView(View.MENU);
-						break;
-					case KeyEvent.VK_F2:
-						changeView(View.CUSTOMER);
-						break;
-					case KeyEvent.VK_F3:
-						changeView(View.HISTORY);
-						break;
-					case KeyEvent.VK_ESCAPE:
-						System.exit(0);
-						break;
-				}
-				return false;
+			public void windowClosed(WindowEvent e) {
+				cleanup();
+				System.exit(0);
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowOpened(WindowEvent e) {
 			}
 		});
-		
-		//Change to dispose or close when called from another frame (ie. the splash screen)
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
 	}
-	
+
 	public void changeView(View view) {
-		//If the frame already has a panel providing its view, remove it
+		// If the frame already has a panel providing its view, remove it
 		if (currentPanel != null) {
 			frame.remove(currentPanel);
 		}
-		
+
 		switch (view) {
-			case MENU:
-				frame.add(menuSearchPanel, BorderLayout.CENTER);
-				currentPanel = menuSearchPanel;
-				break;
-			case CUSTOMER:
-				frame.add(customerPanel, BorderLayout.CENTER);
-				currentPanel = customerPanel;
-				break;
-			case HISTORY:
-				frame.add(orderHistoryPanel, BorderLayout.CENTER);
-				currentPanel = orderHistoryPanel;
-				break;
+		case MENU:
+			frame.add(menuSearchPanel, BorderLayout.CENTER);
+			currentPanel = menuSearchPanel;
+			break;
+		case CUSTOMER:
+			frame.add(customerPanel, BorderLayout.CENTER);
+			currentPanel = customerPanel;
+			break;
+		case HISTORY:
+			frame.add(orderHistoryPanel, BorderLayout.CENTER);
+			currentPanel = orderHistoryPanel;
+			break;
 		}
-		currentPanel.grabFocus(); //This method should be overrided to pass on focus to the search box
-		currentPanel.revalidate(); //Check if the panel has all its components loaded
-		frame.repaint(); //Repaint the frame and all its components
+		currentPanel.grabFocus(); // This method should be overrided to pass on
+									// focus to the search box
+		currentPanel.revalidate(); // Check if the panel has all its components
+									// loaded
+		frame.repaint(); // Repaint the frame and all its components
 	}
-	
+
 	public OrderSummary getOrderSummary() {
 		return orderSummary;
 	}
 
-	//Fired whenever a button in ButtonPanel is pressed
+	// Fired whenever a button in ButtonPanel is pressed
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton src = (JButton)e.getSource();
+		JButton src = (JButton) e.getSource();
 
 		if (src == buttonPanel.menu) {
 			changeView(View.MENU);
-		}
-		else if (src == buttonPanel.customer) {
+		} else if (src == buttonPanel.customer) {
 			changeView(View.CUSTOMER);
-		}
-		else if (src == buttonPanel.history) {
+		} else if (src == buttonPanel.history) {
 			changeView(View.HISTORY);
 		}
 	}
