2f71c5df398885cfa8d63a542fe57e172706d15e
David M <davidmy@stud.ntnu.no>
2011-10-20 15:34:06 +0200

    DBReset now deletes the db file before resetting the data.

--- order_system/src/ntnu/it1901/gruppe4/db/DBReset.java
+++ order_system/src/ntnu/it1901/gruppe4/db/DBReset.java
@@ -1,5 +1,7 @@
 package ntnu.it1901.gruppe4.db;
 
+import java.io.File;
+
 /**
  * Run this once if you want to reset the database. (PS: If your db schema is
  * deprecated, you still have to delete data.db
@@ -9,6 +11,7 @@ package ntnu.it1901.gruppe4.db;
 
 public class DBReset {
 	public static void main(String args[]) {
+		(new File("./data.db")).delete();
 		DataAPI.open("./data.db");
 		DataAPI.clearDatabase();
 		DataAPI.createExampleData();
