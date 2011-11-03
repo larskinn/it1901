c1ad6f9e98227b38635db9bc6417e0e4375d99ff
David M <davidmy@stud.ntnu.no>
2011-11-03 14:35:04 +0100

    Moar Javadoc

--- order_system/src/ntnu/it1901/gruppe4/gui/ordergui/ButtonPanel.java
+++ order_system/src/ntnu/it1901/gruppe4/gui/ordergui/ButtonPanel.java
@@ -8,10 +8,24 @@ import javax.swing.JPanel;
 
 import ntnu.it1901.gruppe4.gui.Layout;
 
+/**
+ * A panel of 3 buttons, as used in the bottom of {@Link OrderWindow}.
+ * The buttons are Menu (F1), Customer (F2) and Order History (F3).
+ * 
+ * @author LeoMartin
+ * 
+ */
 public class ButtonPanel extends JPanel {
 	final JButton menu, customer, history;
-	
-	public ButtonPanel(ActionListener...buttonListeners) {
+
+	/**
+	 * Public constructor.
+	 * 
+	 * @param buttonListeners
+	 *            A list of {@Link ActionListener} objects to bind to the
+	 *            three buttons.
+	 */
+	public ButtonPanel(ActionListener... buttonListeners) {
 		menu = new JButton("Meny (F1)");
 		customer = new JButton("Kunde (F2)");
 		history = new JButton("Order History (F3)");
@@ -20,11 +34,11 @@ public class ButtonPanel extends JPanel {
 			menu.addActionListener(listener);
 			customer.addActionListener(listener);
 			history.addActionListener(listener);
-		}	
+		}
 		menu.setPreferredSize(Layout.buttonSize);
 		customer.setPreferredSize(Layout.buttonSize);
 		history.setPreferredSize(Layout.buttonSize);
-		
+
 		setLayout(new GridLayout(1, 4));
 		add(menu);
 		add(customer);
