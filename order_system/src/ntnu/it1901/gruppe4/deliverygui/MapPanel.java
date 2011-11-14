36f1c02ed51a863fa8bce561ea1126eab47077b3
Lars Kinn Ekroll <larskinn@gmail.com>
2011-11-14 17:08:15 +0100

    Added fancy icons in the map. Refactored some MapPanel insanity.

--- order_system/src/ntnu/it1901/gruppe4/deliverygui/MapPanel.java
+++ order_system/src/ntnu/it1901/gruppe4/deliverygui/MapPanel.java
@@ -6,45 +6,39 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import ntnu.it1901.gruppe4.db.Address;
+import ntnu.it1901.gruppe4.db.Settings;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
- * This class is the Panel where the Google Static API Map is implemented. It
- * takes an address given to it by {@link Address} and puts it in the static
- * URL,<br>
- * generating a new picture each time it is called.
+ * This class is the Panel where the Google Static API Map is implemented.
+ * <p>
+ * It takes an address given to it by {@link Address} and puts it in the static
+ * URL, generating a new picture each time it is called.
  * 
- * @author Morten, Leo
+ * @author Morten
+ * @author Leo
+ * @author Lars
  * 
  */
 
 public class MapPanel extends JPanel {
-
-	String address = "";
-	String modAddress = address.replace(' ', '+');
-	String newAddress = "|&markers=color:red|" + modAddress + ",Trondheim";
+	private static final String RESTAURANT_ADDRESS = Settings.getRestaurantAddress().replace(' ', '+');
+	private static final String DESTINATION_ICON = "http://chart.apis.google.com/chart?chst=d_map_pin_icon%26chld=glyphish_house%257C00FF00";
+	private static final String RESTAURANT_ICON = "http://chart.apis.google.com/chart?chst=d_map_pin_icon%26chld=glyphish_fork-and-knife%257CFF0000";
 
 	public MapPanel() {
-
-		String staticAddress = address.replace(' ', '+');
-		String newAddress = "|&markers=color:red|" + staticAddress
-				+ ",Trondheim";
 
 		BufferedImage image;
 		try {
 			image = ImageIO
 					.read(new URL(
-							"http://maps.google.com/maps/api/staticmap?"
-									+ staticAddress
-									+ ",Trondheim&size=500x500&markers=color:green|Munkegata,Trondheim"
-									+ newAddress + "&sensor=false"));
+							"http://maps.google.com/maps/api/staticmap?size=500x500&markers=icon:"
+							+ RESTAURANT_ICON + "|" + RESTAURANT_ADDRESS + "&sensor=false"));
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			image = null;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			image = null;
 		}
 		if (image != null) {
@@ -55,23 +49,19 @@ public class MapPanel extends JPanel {
 
 	public void setAddress(Address address) {
 		removeAll();
-		String staticAddress = address.getAddressLine().replace(' ', '+');
-		String markerAddress = "|&markers=color:red|" + staticAddress
-				+ ",Trondheim";
+		String destinationAddress = address.getAddressLine().replace(' ', '+') + ",Trondheim";
 
 		BufferedImage image = null;
 		try {
 			image = ImageIO
 					.read(new URL(
-							"http://maps.google.com/maps/api/staticmap?"
-									+ staticAddress
-									+ ",Trondheim&size=500x500&markers=color:green|Munkegata,Trondheim"
-									+ markerAddress + "&sensor=false"));
+							"http://maps.google.com/maps/api/staticmap?size=500x500"
+							+ "&markers=icon:" + RESTAURANT_ICON + "|" + RESTAURANT_ADDRESS
+							+ "&markers=icon:" + DESTINATION_ICON + "|" + destinationAddress
+							+ "&sensor=false"));
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		JLabel label = new JLabel(new ImageIcon(image));
