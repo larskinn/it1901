b70fb9e5da38d6905072a26b475119cc2faf9529
Leo <leo.priv@gmail.com>
2011-11-16 19:42:41 +0100

    Splash screen split into four parts.

--- order_system/src/ntnu/it1901/gruppe4/Main.java
+++ order_system/src/ntnu/it1901/gruppe4/Main.java
@@ -1,5 +1,6 @@
 package ntnu.it1901.gruppe4;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
@@ -7,22 +8,28 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
 import javax.sound.sampled.ReverbType;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import ntnu.it1901.gruppe4.chefgui.ChefWindow;
 import ntnu.it1901.gruppe4.db.DBReset;
 import ntnu.it1901.gruppe4.deliverygui.DeliveryWindow;
+import ntnu.it1901.gruppe4.gui.ConfigWindow;
 import ntnu.it1901.gruppe4.gui.ordergui.OrderWindow;
 
 /**
@@ -54,52 +61,48 @@ public class Main {
  * @author David M.
  * @author Leo
  * @author Morten
- * 
  */
 class SplashScreen extends JFrame implements ActionListener {
 	private JButton btnOrder;
 	private JButton btnChef;
 	private JButton btnDelivery;
 	private JButton btnReset;
-	private JPanel contentPanel;
-	private static Image splashImage;
+	private JLabel lblMadeBy;
 	
 	SplashScreen() {
-		btnOrder = new JButton();
-		btnChef = new JButton();
-		btnDelivery = new JButton();
+		Icon iconOrder = new ImageIcon(getClass().getResource("/images/SplashScreenNW.png"));
+		Icon iconChef = new ImageIcon(getClass().getResource("/images/SplashScreenNE.png")); 
+		Icon iconDelivery = new ImageIcon(getClass().getResource("/images/SplashScreenSW.png"));
+		Icon iconMadeBy = new ImageIcon(getClass().getResource("/images/SplashScreenSE.png"));
+		btnOrder = new JButton(iconOrder);
+		btnChef = new JButton(iconChef);
+		btnDelivery = new JButton(iconDelivery);
+		lblMadeBy = new JLabel(iconMadeBy);
 		btnReset = new JButton();
-		splashImage = Toolkit.getDefaultToolkit().createImage
-				(getClass().getResource("/images/SplashScreen.png"));
-		
-		contentPanel = new JPanel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-				super.paintComponents(g);
-
-				//Paint the splash image on the background
-				g.drawImage(splashImage, 0, 0, splashImage.getWidth(null), splashImage.getHeight(null), null);
-			}
-		};
 		
 		setTitle("Gruppe 4 pizza");
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		contentPanel.setLayout(new GridLayout(2, 2));
+		setLayout(new GridLayout(2, 2));
 		
 		btnOrder.addActionListener(this);
 		btnChef.addActionListener(this);
 		btnDelivery.addActionListener(this);
 		btnReset.addActionListener(this);
 		
-		btnOrder.setContentAreaFilled(false);
-		btnChef.setContentAreaFilled(false);
-		btnDelivery.setContentAreaFilled(false);
-		btnReset.setContentAreaFilled(false);
+		//We want the buttons to have the exact size of their contained images
+		btnOrder.setPreferredSize(new Dimension(iconOrder.getIconWidth(), iconOrder.getIconHeight()));
+		btnChef.setPreferredSize(new Dimension(iconChef.getIconWidth(), iconChef.getIconHeight()));
+		btnDelivery.setPreferredSize(new Dimension(iconDelivery.getIconWidth(), iconDelivery.getIconHeight()));
+		lblMadeBy.setPreferredSize(new Dimension(iconMadeBy.getIconWidth(), iconMadeBy.getIconHeight()));
 		
-		contentPanel.add(btnOrder);
-		contentPanel.add(btnChef);
-		contentPanel.add(btnDelivery);
-		add(contentPanel);
+		add(btnOrder);
+		add(btnChef);
+		add(btnDelivery);
+		add(lblMadeBy);
+		pack();
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
 
 		addWindowListener(new WindowAdapter() {
 			@Override
@@ -108,28 +111,6 @@ class SplashScreen extends JFrame implements ActionListener {
 				System.exit(0);
 			}
 		});
-		
-		
-		int timeout = 0;
-		
-		//Wait until Java has loaded the splash screen picture
-		while(!splashScreenLoaded()) {
-			try {
-				Thread.sleep(80);
-				timeout += 80;
-				
-				if (timeout > 2000) {
-					System.err.println("CRITICAL ERROR: The program timed out while attempting to load 'SplashScreen.png'");
-				}
-			} catch (InterruptedException e) {}
-		}
-		
-		setSize(splashImage.getWidth(null), splashImage.getHeight(null));
-		setVisible(true);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		contentPanel.revalidate();
-		contentPanel.repaint();
 	}
 
 	@Override
@@ -159,15 +140,5 @@ class SplashScreen extends JFrame implements ActionListener {
 				DBReset.resetDB();
 			}
 		}
-	}
-	
-	/**
-	 * Java does not immediately load pictures into RAM.
-	 * This method returns whether or not the splash screen has been loaded.
-	 * 
-	 * @return True if the splash screen has been loaded into RAM.
-	 */
-	public boolean splashScreenLoaded() {
-		return splashImage.getWidth(null) != -1;
 	}
 }
