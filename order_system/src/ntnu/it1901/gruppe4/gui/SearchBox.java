d0b35d38162995be1113874a19632a319289612f
LeoWestby <leo.priv@gmail.com>
2011-11-20 22:10:37 +0100

    Removed an unused KeyListener.

--- order_system/src/ntnu/it1901/gruppe4/gui/SearchBox.java
+++ order_system/src/ntnu/it1901/gruppe4/gui/SearchBox.java
@@ -1,23 +1,18 @@
 package ntnu.it1901.gruppe4.gui;
 
 import java.awt.Dimension;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 import javax.swing.JTextField;
 
-
+/**
+ * A general text field with a large, bold font.
+ * Its width is unlimited and its height is set to that of its font's.
+ * 
+ * @author Leo
+ */
 public class SearchBox extends JTextField {
 	public SearchBox() {
 		setFont(Layout.searchBoxFont);
-		
-		//To prevent this component's height from growing
 		setMaximumSize(new Dimension(Short.MAX_VALUE, getPreferredSize().height));
-		
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-			}
-		});
 	}
 }
